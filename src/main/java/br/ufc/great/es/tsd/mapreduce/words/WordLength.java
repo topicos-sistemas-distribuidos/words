db10ff9c627e5a5238e56192c2e8465e54f50c9a
armandossrecife <armando.sousa@gmail.com>
2019-05-22 11:06:44 -0300

    Criado um primeiro esboco do WordLength.

--- src/main/java/br/ufc/great/es/tsd/mapreduce/words/WordLength.java
+++ src/main/java/br/ufc/great/es/tsd/mapreduce/words/WordLength.java
@@ -22,22 +22,29 @@ public class WordLength {
       
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
       StringTokenizer itr = new StringTokenizer(value.toString());
+      
       while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
+  		String myKey = itr.nextToken();
+  		myKey = myKey.replaceAll ( "[^A-Za-z0-9]",""); 
+  		int myValue = myKey.length();
+  		one.set(myValue);
+        word.set(myKey);
         context.write(word, one);
       }
+      
     }
+    
   }
   
-  public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+  public static class IntSizeReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
     private IntWritable result = new IntWritable();
 
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-      int sum = 0;
+      int size = 0;
       for (IntWritable val : values) {
-        sum += val.get();
+        size = val.get();
       }
-      result.set(sum);
+      result.set(size);
       context.write(key, result);
     }
   }
@@ -52,8 +59,8 @@ public class WordLength {
     Job job = Job.getInstance(conf, "word count");
     job.setJarByClass(WordLength.class);
     job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
-    job.setReducerClass(IntSumReducer.class);
+    job.setCombinerClass(IntSizeReducer.class);
+    job.setReducerClass(IntSizeReducer.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
     for (int i = 0; i < otherArgs.length - 1; ++i) {
