71b243a78cea451119432d14626e155a52b8a36d
Armando Soares Sousa <armando.sousa@gmail.com>
2019-05-22 00:05:30 -0300

    Update WordAverage.java
    Foi atualizado o comportamento dos metodos map e reduce para reproduzir a media dos tamanhos das palavras dos textos.

--- src/main/java/br/ufc/great/es/tsd/mapreduce/words/WordAverage.java
+++ src/main/java/br/ufc/great/es/tsd/mapreduce/words/WordAverage.java
@@ -17,38 +17,53 @@ import org.apache.hadoop.util.GenericOptionsParser;
 public class WordAverage {
 
   public static class TokenizerAverageMapper extends Mapper<Object, Text, Text, IntWritable>{
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-      
-    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        context.write(word, one);
-      }
-    }
+    String line = new String();
+    Text firstLetter = new Text();
+    IntWritable wordLength = new IntWritable();
+
+    @Override
+    public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+
+        line = value.toString();
+
+        for (String word : line.split("\\W+")){
+            if (word.length() > 0) {
+                // ---------------------------------------------
+                // firstLetter assignment
+                firstLetter.set(String.valueOf(word.charAt(0)));
+                // ---------------------------------------------
+                wordLength.set(word.length());
+                context.write(firstLetter, wordLength);
+            }
+        }
   }
   
   public static class AverageWordLength extends Reducer<Text,IntWritable,Text,IntWritable> {
     private IntWritable result = new IntWritable();
 
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
-      }
-      result.set(sum);
-      context.write(key, result);
+    public void reduce(Text key, Iterable<IntWritable> values, Context output) throws IOException, InterruptedException {
+      int sum =0; 
+            int count =0; 
+            int Average =0;
+            for(IntWritable val:values) {
+                sum += val.get(); 
+                 count = count+1;
+            }
+            Average = sum/count;
+            output.write(Key,new IntWritable(Average));
     }
   }
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
     if (otherArgs.length < 2) {
       System.err.println("Usage: wordaverage <in> [<in>...] <out>");
       System.exit(2);
     }
+    
     Job job = Job.getInstance(conf, "word average");
     job.setJarByClass(WordAverage.class);
     job.setMapperClass(TokenizerAverageMapper.class);
@@ -56,9 +71,11 @@ public class WordAverage {
     job.setReducerClass(AverageWordLength.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
+    
     for (int i = 0; i < otherArgs.length - 1; ++i) {
       FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
     }
+    
     FileOutputFormat.setOutputPath(job,
       new Path(otherArgs[otherArgs.length - 1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
