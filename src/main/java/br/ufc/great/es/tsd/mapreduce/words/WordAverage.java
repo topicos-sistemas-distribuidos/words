fc94f9657897f416319e44b509e632286bb0b9e1
armandossrecife <armando.sousa@gmail.com>
2019-05-22 11:41:54 -0300

    Correcao do calculo da medida do tamanho das palavras.

--- src/main/java/br/ufc/great/es/tsd/mapreduce/words/WordAverage.java
+++ src/main/java/br/ufc/great/es/tsd/mapreduce/words/WordAverage.java
@@ -6,12 +6,10 @@ import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
@@ -39,16 +37,21 @@ public class WordAverage {
 	}
 
 	public static class AverageWordLength extends Reducer<Text,IntWritable,Text,IntWritable> {   
-		public void reduce(Text key, Iterable<IntWritable> values, Context output) throws IOException, InterruptedException {
+		private IntWritable result = new IntWritable();
+		
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 			int sum =0; 
 			int count =0; 
 			int Average =0;
+			
 			for(IntWritable val:values) {
 				sum += val.get(); 
 				count = count+1;
 			}
+			
 			Average = sum/count;
-			output.write(key,new IntWritable(Average));
+			result.set(Average);
+			context.write(key,result);
 		}
 	}
 
