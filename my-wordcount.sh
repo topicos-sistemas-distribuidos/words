ff510f2404d6634ca972d5e6adf98c8a72d5b349
armandossrecife <armando.sousa@gmail.com>
2019-05-28 13:14:11 -0300

    Correcao dos scripts para nao mostrar os arquivos intermediarios.

--- my-wordcount.sh
+++ my-wordcount.sh
@@ -17,13 +17,6 @@ hdfs dfs -put ./input$1/* input$1
 # run wordcount 
 hadoop jar /root/words/target/words-1.0.0.jar input$1 output$1
 
-# print the input files
-echo -e "\ninput file1.txt:"
-hdfs dfs -cat input$1/file1.txt
-
-echo -e "\ninput file2.txt:"
-hdfs dfs -cat input$1/file2.txt
-
 # print the output of wordcount
 echo -e "\nwordcount output:"
 hdfs dfs -cat output$1/part-r-00000
