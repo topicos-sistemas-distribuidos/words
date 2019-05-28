4b779fee15d3d826b9f056eb03f176606563ff08
armandossrecife <armando.sousa@gmail.com>
2019-05-28 10:34:38 -0300

    Alterada a chave de comparacao do Text para IntWritable.

--- src/main/java/br/ufc/great/es/tsd/mapreduce/words/WordLength.java
+++ src/main/java/br/ufc/great/es/tsd/mapreduce/words/WordLength.java
@@ -80,14 +80,14 @@ public class WordLength {
 	 */
 	public static class DescendingKeyComparator extends WritableComparator {
 		protected DescendingKeyComparator() {
-			super(Text.class, true);
+			super(IntWritable.class, true);
 		}
 
 		@SuppressWarnings("rawtypes")
 		@Override
 		public int compare(WritableComparable w1, WritableComparable w2) {
-			LongWritable key1 = (LongWritable) w1;
-			LongWritable key2 = (LongWritable) w2;          
+			IntWritable key1 = (IntWritable) w1;
+			IntWritable key2 = (IntWritable) w2;          
 			return -1 * key1.compareTo(key2);
 		}
 	}
