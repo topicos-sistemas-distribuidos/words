c3b44d264a5cc711b02844c5ad39218de94324f4
armandossrecife <armando.sousa@gmail.com>
2019-05-30 10:15:29 -0300

    Corrigido de text para myword no WordReduce.

--- src/main/java/br/ufc/great/es/tsd/mapreduce/words/orderby/WordReducer.java
+++ src/main/java/br/ufc/great/es/tsd/mapreduce/words/orderby/WordReducer.java
@@ -6,11 +6,11 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class WordReducer extends Reducer<MyWord, IntWritable, Text, IntWritable>{
+public class WordReducer extends Reducer<MyWord, IntWritable, MyWord, IntWritable>{
 
 	public void reduce(MyWord key, Iterable<IntWritable> value, Context context) throws IOException, InterruptedException{
 		for(IntWritable lsize:value){
-		context.write(key.getFname(), lsize);
+		context.write(key, lsize);
 	}
 
 }
